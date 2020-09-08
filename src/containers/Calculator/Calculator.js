67981c1fca530ac8414ee46406f102ef45aebfd4
lokesh26 <lokesh.agg26@gmail.com>
2020-09-08 12:36:57 +1000

    Added Display

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
-
 import classes from './Calculator.module.css';
+import Display from '../../components/Display/Display';
 
 class Calculator extends Component{
     render(){
         return(
             <div className={classes.Calculator}>
-                <div>Display</div>
-                <div>Button Panel</div>
+                <Display>0</Display>
+                <div className={classes.ButtonPanel}>
+                    <div className={classes.ScientificPanel}>Scientific</div>
+                    <div className={classes.DigitalPanel}>Digital</div>
+                </div>
             </div>
         );
     }
